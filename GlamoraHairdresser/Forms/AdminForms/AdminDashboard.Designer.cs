c87b7efcf7ccfbeccba363cc851cf83c6e9cdd6e
lina <Lina.alsawadi2001@gmail.com>
2025-10-18 14:22:13 +0300

    AdminDashboard

--- GlamoraHairdresser/Forms/AdminForms/AdminDashboard.Designer.cs
+++ GlamoraHairdresser/Forms/AdminForms/AdminDashboard.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminDashboard";
+            SalonBtn = new Button();
+            WorkerBtn = new Button();
+            ProfitBtn = new Button();
+            CustomerBtn = new Button();
+            SuspendLayout();
+            // 
+            // SalonBtn
+            // 
+            SalonBtn.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            SalonBtn.Location = new Point(192, 76);
+            SalonBtn.Name = "SalonBtn";
+            SalonBtn.Size = new Size(118, 56);
+            SalonBtn.TabIndex = 8;
+            SalonBtn.Text = "Salon";
+            SalonBtn.UseVisualStyleBackColor = true;
+            SalonBtn.Click += SalonBtn_Click;
+            // 
+            // WorkerBtn
+            // 
+            WorkerBtn.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            WorkerBtn.Location = new Point(363, 76);
+            WorkerBtn.Name = "WorkerBtn";
+            WorkerBtn.Size = new Size(118, 56);
+            WorkerBtn.TabIndex = 9;
+            WorkerBtn.Text = "Worker";
+            WorkerBtn.UseVisualStyleBackColor = true;
+            WorkerBtn.Click += WorkerBtn_Click;
+            // 
+            // ProfitBtn
+            // 
+            ProfitBtn.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            ProfitBtn.Location = new Point(363, 183);
+            ProfitBtn.Name = "ProfitBtn";
+            ProfitBtn.Size = new Size(118, 56);
+            ProfitBtn.TabIndex = 10;
+            ProfitBtn.Text = "Profit";
+            ProfitBtn.UseVisualStyleBackColor = true;
+            ProfitBtn.Click += ProfitBtn_Click;
+            // 
+            // CustomerBtn
+            // 
+            CustomerBtn.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            CustomerBtn.Location = new Point(192, 183);
+            CustomerBtn.Name = "CustomerBtn";
+            CustomerBtn.Size = new Size(118, 56);
+            CustomerBtn.TabIndex = 11;
+            CustomerBtn.Text = "Customer";
+            CustomerBtn.UseVisualStyleBackColor = true;
+            CustomerBtn.Click += CustomerBtn_Click;
+            // 
+            // AdminDashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CustomerBtn);
+            Controls.Add(ProfitBtn);
+            Controls.Add(WorkerBtn);
+            Controls.Add(SalonBtn);
+            Name = "AdminDashboard";
+            Text = "AdminDashboard";
+            ResumeLayout(false);
         }
 
         #endregion
+        private Button SalonBtn;
+        private Button WorkerBtn;
+        private Button ProfitBtn;
+        private Button CustomerBtn;
     }
 }
--- GlamoraHairdresser/Forms/AdminForms/AdminDashboard.cs
+++ GlamoraHairdresser/Forms/AdminForms/AdminDashboard.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace GlamoraHairdresser.WinForms.Forms.AdminForms
 {
-    public partial class AdminDashboard: Form
+    public partial class AdminDashboard : Form
     {
         public AdminDashboard()
         {
             InitializeComponent();
+        }
+
+        private void SalonBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WorkerBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CustomerBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProfitBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
